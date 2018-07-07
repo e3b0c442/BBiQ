cd03ccd02eb01a0fe135f69e43c62b39c650c60a
Nick Meyer <bierdybard@gmail.com>
2018-07-06 20:51:13 -0500

    view

---
+++ prototype/src/Arduino/view.cpp
@@ -0,0 +1,105 @@
+#include <string.h>
+#include <LiquidCrystal.h>
+#include "event.h"
+#include "pin.h"
+#include "version.h"
+#include "view.h"
+
+LiquidCrystal LCD(    
+    PIN_LCD_RS, 
+    PIN_LCD_ENABLE, 
+    PIN_LCD_D4, 
+    PIN_LCD_D5,
+    PIN_LCD_D6,
+    PIN_LCD_D7
+);
+
+typedef struct {
+    int x;
+    int y;
+    int width;
+    char* prefix;
+    char* value;
+    char* postfix;
+    bool selectable;
+    bool selected;
+    bool blank;
+} ViewField;
+
+const unsigned long VIEW_INTRO_DURATION = 5000UL;
+
+bool viewIntro = true;
+bool viewDirty = false;
+
+ViewField viewNameField = { 0, 0, 10, "", "", "", true, true, false };
+ViewField viewTempField = { 12, 0, 4, "", "", "\xdf", false, false, false };
+ViewField viewLowField = { 0, 1, 7, "L:", "", "", true, false, false };
+ViewField viewHighField = { 0, 1, 7, "L:", "", "", true, false, false };
+
+void _drawField(ViewField* field);
+
+void viewSetup() {
+    LCD.begin(16, 2);
+    LCD.setCursor(0, 0);
+    LCD.print(F("BBiQ v"));
+    LCD.print(BBIQ_VERSION);
+    LCD.setCursor(0, 1);
+    LCD.print(F("Smoke on"));
+}
+
+void viewLoop() {
+    unsigned long ts = millis();
+    if(viewIntro) {
+        if((long)ts - long(VIEW_INTRO_DURATION) > 0) {
+            viewIntro = false;
+            viewDirty = true;
+            LCD.clear();
+        }
+    }
+    
+    if(viewDirty) {
+        _drawField(&viewNameField);
+        _drawField(&viewTempField);
+        _drawField(&viewLowField);
+        _drawField(&viewHighField);
+    }
+}
+
+void _drawField(ViewField* field) {
+    LCD.setCursor(field->x, field->y);
+    LCD.print(field->prefix);
+
+    if(field->selectable) {
+        if(field->selected) {
+            LCD.print(F("["));
+        } else {
+            LCD.print(F(" "));
+        }
+    }
+    
+    byte valueLen = field->width - 2 - strlen(field->prefix) - strlen(field->postfix);
+    char toPrint[valueLen + 1];
+    toPrint[valueLen] = '\0';
+    if(field->blank) {
+        memset(toPrint, ' ', valueLen);
+    } else {
+        byte fieldLen = strlen(field->value);
+        if(fieldLen >= valueLen) {
+            strncpy(toPrint, field->value, valueLen);
+        } else {
+            strcpy(toPrint, field->value);
+            memset(toPrint + fieldLen, ' ', valueLen - fieldLen);
+        }
+    }
+    LCD.print(toPrint);
+
+    if(field->selectable) {
+        if(field->selected) {
+            LCD.print(F("]"));
+        } else {
+            LCD.print(F(" "));
+        }
+    }
+
+    LCD.print(field->postfix);
+}
---
+++ prototype/src/Arduino/view.h
@@ -0,0 +1,10 @@
+#ifndef VIEW_E3B0C442_H
+#define VIEW_E3B0C442_H
+
+#include "event.h"
+
+void viewSetup();
+void viewLoop();
+void viewEventHandler(Event* evt);
+
+#endif // VIEW_E3B0C442_H
