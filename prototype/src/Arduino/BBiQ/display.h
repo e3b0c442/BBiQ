7678a05bf872894c043adf38d596f6bb0b6dae73
Nick Meyer <nick@e3b0c442.io>
2018-07-13 22:20:43 -0500

    remove old source folder

--- prototype/src/Arduino/BBiQ/display.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISPLAY_E3B0C442_H
-#define DISPLAY_E3B0C442_H
-
-#include "event.h"
-
-void displaySetup();
-void displayLoop();
-
-#endif // DISPLAY_E3B0C442_H
--- prototype/src/Arduino/BBiQ/freeMem.h
+++
@@ -1,6 +0,0 @@
-#ifndef FREEMEM_E3B0C442_H
-#define FREEMEM_E3B0C442_H
-
-int freeMemory();
-
-#endif // FREEMEM_E3B0C442_H
