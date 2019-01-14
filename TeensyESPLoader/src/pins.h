80ea5f5ac97973cc1d723b6a7b070ddf8346bb83
Nick <nick@e3b0c442.io>
2019-01-13 19:08:32 -0600

    redo loader

---
+++ TeensyESPLoader/src/pins.h
@@ -0,0 +1,19 @@
+#define ESP_TXD 0
+#define ESP_UART0_TXD 0
+#define ESP_GPIO1 0
+
+#define ESP_RXD 1
+#define ESP_UART0_RXD 1
+#define ESP_GPIO3 1
+
+#define ESP_IO0 2
+#define ESP_GPIO0 2
+
+#define ESP_RST 3
+
+#define MODE_SWITCH 6
+
+#define RESET_BUTTON 7
+
+#define ESP_IO2 9
+#define ESP_UART1_TXD 9
